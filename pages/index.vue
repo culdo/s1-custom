b1e43d90b9e7a37f76ae25c42229ff5876f7656f
George Hu <george_hu@justher.tw>
2023-03-03 15:00:26 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const JSDOM = require('jsdom').JSDOM; 
+
+const { data, pending, refresh } = await useAsyncData(
+  async () => {
+    const res = await fetch(
+      'https://www.saraba1st.com/2b/forum-6-1.html'
+    )
+    const text = await res.text()
+    const dom = new JSDOM(text);
+    const content = dom.window.document.body.innerHTML;
+    console.log(content)
+    return content
+  }
+)
+
+</script>
+
+<template>
+  <div v-html="data">
+  </div>
+</template>
