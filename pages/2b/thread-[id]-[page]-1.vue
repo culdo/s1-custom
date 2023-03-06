9a8c2bf8a6f48075a0849aee14f47826e8ac86c4
George Hu <george_hu@justher.tw>
2023-03-06 12:17:23 +0800

    done thread page rendering

--- pages/2b/thread-[id]-[page]-1.vue
+++ pages/2b/thread-[id]-[page]-1.vue
@@ -1,39 +1,56 @@
 <script setup>
 const JSDOM = require('jsdom').JSDOM; 
 const route = useRoute();
+const isShowImg = ref(false);
+function toggleShowImg() {
+  isShowImg.value = !isShowImg.value;
+}
 
+const postOrigUrl = `https://www.saraba1st.com/2b/thread-${route.params.id}-${route.params.page}-1.html`;
 const { data, pending, refresh } = await useAsyncData(
   async () => {
-    const res = await fetch(
-      `https://www.saraba1st.com/2b/thread-${route.params.id}-${route.params.page}-1.html`
-    )
-    const text = await res.text()
+    const res = await fetch(postOrigUrl);
+    const text = await res.text();
     const dom = new JSDOM(text);
     const posts = dom.window.document.querySelectorAll("[id^=post_]");
     
     let data = [];
-    // posts.forEach(post => {
-    //   if(post.hasChildNodes()) {
-    //     console.log(post.innerText);
-    //     data.push({
-    //       content: post.querySelector("[id^=postmessage_]").innerText,
-    //     })
-    //   }
-    // });
-    console.log(data);
-    return posts
+    posts.forEach((post, i) => {
+      if(post.hasChildNodes()) {
+        let postMsg = post.querySelector("[id^=postmessage_]");
+        post.querySelectorAll("img[id^=aimg_]").forEach((img) => {
+          img.src = img.getAttribute("file");
+        })
+        if(postMsg) {
+          data.push(postMsg)
+        }
+      }
+    });
+    return data
   }
 )
 
 </script>
 
 <template>
-  <div class="m-4 text-slate-600 text-center">
-    <div class="flex" v-for="post in data">
+  <div class="m-4 text-slate-600">
+    <a :href="postOrigUrl" target="_blank" >Orignal Post</a>
+    <button @click="toggleShowImg">{{ isShowImg.value }} Img</button>
+    <div class="flex border-2 my-2" v-for="post in data">
       <!-- {{ post.content }} -->
-      <div v-html="post.innerHTML">
+      <div v-html="post.innerHTML"> 
       </div>
     </div>
   </div>
   
 </template>
+
+<style>
+  img {
+    display: inline;
+  }
+
+  img[id^=aimg_] {
+    width: 800px;
+  }
+</style>
