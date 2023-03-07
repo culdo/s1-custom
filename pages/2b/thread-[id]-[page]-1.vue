7a8d8d4fc31b08c28e219b3d232e7c77e807e1c2
George Hu <wuorsut@gmail.com>
2023-03-07 10:58:40 +0800

    add "Show/Hide Image" button

--- pages/2b/thread-[id]-[page]-1.vue
+++ pages/2b/thread-[id]-[page]-1.vue
@@ -3,9 +3,6 @@ import {parseHTML} from 'linkedom'
 
 const route = useRoute();
 const isShowImg = ref(false);
-function toggleShowImg() {
-  isShowImg.value = !isShowImg.value;
-}
 
 const postOrigUrl = `https://www.saraba1st.com/2b/thread-${route.params.id}-${route.params.page}-1.html`;
 const { data, pending, refresh } = await useAsyncData(
@@ -15,29 +12,49 @@ const { data, pending, refresh } = await useAsyncData(
     const dom = parseHTML(text);
     const posts = dom.window.document.querySelectorAll("[id^=post_]");
     
-    let data = [];
-    posts.forEach((post, i) => {
+    let preprcPosts = [];
+    posts.forEach((post) => {
       if(post.hasChildNodes()) {
         let postMsg = post.querySelector("[id^=postmessage_]");
-        post.querySelectorAll("img[id^=aimg_]").forEach((img) => {
-          img.src = img.getAttribute("file");
-        })
         if(postMsg) {
-          data.push(postMsg)
+          // fill src by file url
+          postMsg.querySelectorAll("img[id^=aimg_]").forEach((img) => {
+            img.src = img.getAttribute("file");
+            img.style.display = "none";
+          })
+          preprcPosts.push(postMsg);
         }
       }
     });
-    return data
+    return preprcPosts
   }
 )
+
+function toggleShowImg() {
+  isShowImg.value = !isShowImg.value;
+  let preprcPosts = [];
+  data.value.forEach(postMsg => {
+    postMsg.querySelectorAll("img[id^=aimg_]").forEach((img) => {
+      if(isShowImg.value){
+        img.style.display = "inline";
+      }else{
+        img.style.display = "none";
+      }
+    })
+    preprcPosts.push(postMsg);
+  });
+  data.value = preprcPosts;
+}
 
 </script>
 
 <template>
   <div class="m-4 text-slate-600">
-    <a :href="postOrigUrl" target="_blank" >Orignal Post</a>
-    <button @click="toggleShowImg">{{ isShowImg.value }} Img</button>
-    <div class="flex border-2 my-2" v-for="post in data">
+    <div class="flex gap-4">
+      <a :href="postOrigUrl" target="_blank" >Orignal Post</a>
+      <button @click="toggleShowImg">{{ isShowImg ? "Hide" : "Show" }} Img</button>
+    </div>
+    <div class="border-2 my-2" v-for="post in data">
       <!-- {{ post.content }} -->
       <div v-html="post.innerHTML"> 
       </div>
