c2d17510b02952649dfeb3017dc45000f0e283bd
George Hu <wuorsut@gmail.com>
2023-03-07 16:34:40 +0800

    add threads infinite scrolling,

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,57 @@
 <script setup>
 import {parseHTML} from 'linkedom'
+import InfiniteLoading from "v3-infinite-loading";
+let allThreads = ref([]);
+
 const config = useRuntimeConfig()
 
-const { data, pending, refresh } = await useAsyncData(
-  async () => {
-    const res = await fetch(
-      'https://www.saraba1st.com/2b/forum-6-1.html'
-    )
-    const text = await res.text()
-    const dom = parseHTML(text)
-    const threads = dom.window.document.querySelectorAll("[id^=normalthread]")
+let page = 1;
+let threadOrigUrl;
+
+const load = async $state => {
+  console.log("loading...");
+
+  try {
+    threadOrigUrl = `https://www.saraba1st.com/2b/forum-6-${page}.html`
     
-    let data = []
+    const response = await fetch(threadOrigUrl);
+    const text = await response.text();
+    const dom = parseHTML(text);
+    
+    const threads = dom.window.document.querySelectorAll("[id^=normalthread]")
+    console.log(threads)
+    let procedThreads = []
     threads.forEach(thread => {
-      data.push({
+      procedThreads.push({
         title: thread.querySelector("a.s.xst").textContent,
         link: thread.querySelector("a.s.xst").href.split(".html")[0],
         repliesNum: thread.querySelector(".xi2").textContent,
       })
     })
-    // console.log(data)
-    return data
+    allThreads.value.push(...procedThreads);
+
+    // one page has 50 threads - 2 pinned threads
+    if (procedThreads.length < 48) {
+      $state.complete();
+    } else {
+      $state.loaded();
+    }
+    page++;
+  } catch (error) {
+    $state.error();
   }
-)
+}
+
 
 </script>
 
 <template>
   <div class="m-4 text-slate-600 text-center">
-    <div class="flex" v-for="thread in data">
+    <div class="flex" v-for="thread in allThreads">
       <a class="w-4/5" :href="config.urlBase + '2b/'+thread.link">{{ thread.title }}</a>
       <div class="w-1/5">{{ thread.repliesNum }}</div>
     </div>
+    <InfiniteLoading @infinite="load" />
   </div>
   
 </template>
