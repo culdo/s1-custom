37a13ff53d7c32988383781c4939a6b48900d0ac
George Hu <wuorsut@gmail.com>
2023-04-10 17:33:50 +0800

    refactor threadList url

--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,4 @@
 export const apiBaseUrl = "https://www.saraba1st.com/2b/api/mobile/";
 
 export const getApiPostList = (tid, page) => apiBaseUrl + `index.php?module=viewthread&version=1&ppp=30&tid=${tid}&page=${page}`;
+export const getApiThreadList = (fid, page) => apiBaseUrl + `index.php?module=forumdisplay&version=4&filter=typeid&tpp=50&fid=${fid}&page=${page}`;
