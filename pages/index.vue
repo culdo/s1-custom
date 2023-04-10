ac04096b2d3a973686146bd1388c5c4280191c6e
George Hu <wuorsut@gmail.com>
2023-04-10 17:32:42 +0800

    refactor thread list

--- pages/index.vue
+++ pages/index.vue
@@ -8,26 +8,19 @@ const config = useRuntimeConfig()
 let page = 1;
 let threadOrigUrl;
 
+const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
+
 const load = async $state => {
   console.log("loading...");
 
   try {
-    threadOrigUrl = apiBaseUrl + `index.php?module=forumdisplay&version=4&filter=typeid&tpp=&fid=6&page=${page}`
+    threadOrigUrl = getApiThreadList(6, page);
     
     const response = await fetch(threadOrigUrl);
     const data = await response.json();
-    const dom = parseHTML(text);
-    
-    const threads = dom.window.document.querySelectorAll("[id^=normalthread_],[id^=stickthread_]")
-    let procedThreads = []
-    threads.forEach(thread => {
-      procedThreads.push({
-        title: thread.querySelector("a.s.xst").textContent,
-        link: thread.querySelector("a.s.xst").href.split(".html")[0],
-        repliesNum: thread.querySelector(".xi2").textContent,
-      })
-    })
-    allThreads.value.push(...procedThreads);
+    console.log(data);
+
+    allThreads.value = data.Variables.forum_threadlist;
 
     // There are 50 threads in one page
     if (procedThreads.length < 50) {
@@ -47,7 +40,7 @@ const load = async $state => {
 <template>
   <div class="m-4 text-slate-600 text-center">
     <div class="flex" v-for="thread in allThreads">
-      <a class="w-4/5" :href="config.urlBase + '2b/'+thread.link">{{ thread.title }}</a>
+      <a class="w-4/5" :href="getThreadLink(thread)">{{ thread.subject }}</a>
       <div class="w-1/5">{{ thread.repliesNum }}</div>
     </div>
     <InfiniteLoading @infinite="load" />
