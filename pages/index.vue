891b6e3a58b4fcafcb2cdb3f452467e3e81825a0
George Hu <wuorsut@gmail.com>
2023-04-17 18:22:08 +0800

    WIP: two direction infinite scrolling

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <script setup>
 import InfiniteLoading from "v3-infinite-loading";
+import "v3-infinite-loading/lib/style.css";
 let allThreads = ref([]);
 
 const config = useRuntimeConfig()
 
-let page = 1;
+let page = localStorage.getItem("thread-list-page") || 1;
 let threadOrigUrl;
 
 const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
 
-const load = async $state => {
+const load = async ($state, isTop) => {
   console.log("loading...");
 
   try {
@@ -19,15 +20,23 @@ const load = async $state => {
     const data = await response.json();
     console.log(data);
 
-    allThreads.value.push(...data.Variables.forum_threadlist);
 
+    if(isTop){
+      if(page > 0 || allThreads.value.length > 0 ) {
+        allThreads.value.unshift(...data.Variables.forum_threadlist);
+        page--;
+      }
+    }else{
+      allThreads.value.push(...data.Variables.forum_threadlist);
+      page++;
+    }
     // There are 50 threads in one page if it's not end
-    if (data.Variables.forum_threadlist < 50) {
+    if (data.Variables.forum_threadlist < 50 || page < 1) {
       $state.complete();
     } else {
       $state.loaded();
+      localStorage.setItem("thread-list-page", page);
     }
-    page++;
   } catch (error) {
     $state.error();
   }
@@ -37,11 +46,12 @@ const load = async $state => {
 </script>
 
 <template>
-  <div class="m-4 text-slate-600">
-    <div class="flex justify-center" v-for="thread in allThreads">
+  <div class="m-4 text-slate-600 flex flex-col items-center" >
+    <InfiniteLoading top=true @infinite="load($event, true)" />
+    <div v-for="thread in allThreads">
       <a :href="getThreadLink(thread)">{{ thread.subject }}&nbsp;&nbsp;<b>{{ thread.replies }}</b></a>
     </div>
-    <InfiniteLoading @infinite="load" />
+    <InfiniteLoading @infinite="load($event, false)" />
   </div>
   
 </template>
