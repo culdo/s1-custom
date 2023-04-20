cf24a7c86396959f904de9016d43d11da9d69db6
George Hu <wuorsut@gmail.com>
2023-04-20 09:35:36 +0800

    forumlist: fix variable names

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@ import "v3-infinite-loading/lib/style.css";
 let allThreads = ref([]);
 
 let page = parseInt(localStorage.getItem("thread-list-page")) || 1;
-let topPage = page - 1;
-let bottomPage = page;
+let scrollTopPage = page - 1;
+let scrollBottomPage = page;
 let threadOrigUrl;
 
 const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
@@ -15,9 +15,9 @@ const load = async ($state, isTop) => {
   console.log("loading...");
 
   if(isTop){
-    page = topPage;
+    page = scrollTopPage;
   }else{
-    page = bottomPage;
+    page = scrollBottomPage;
   }
   try {
     localStorage.setItem("thread-list-page", page);
@@ -33,10 +33,10 @@ const load = async ($state, isTop) => {
     } else {
       if(isTop){
         allThreads.value.unshift(data.Variables.forum_threadlist);
-        topPage = page - 1;
+        scrollTopPage = page - 1;
       }else{
         allThreads.value.push(data.Variables.forum_threadlist);
-        bottomPage = page + 1;
+        scrollBottomPage = page + 1;
       }
       $state.loaded();
       loading = false;
@@ -51,7 +51,7 @@ const load = async ($state, isTop) => {
 
 <template>
   <div class="m-4 text-slate-600 flex flex-col items-center" >
-    <InfiniteLoading v-if="topPage > 0 && !loading" top=true @infinite="load($event, true)" />
+    <InfiniteLoading v-if="scrollTopPage > 0 && !loading" top=true @infinite="load($event, true)" />
     <div class="flex flex-col items-center border-b-2 w-full" v-for="block in allThreads">
       <div v-for="thread in block">
         <a :href="getThreadLink(thread)">{{ thread.subject }}&nbsp;&nbsp;<b>{{ thread.replies }}</b></a>
