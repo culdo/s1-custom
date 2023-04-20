68fd1fc58e1bf32470c52703bd11ac785d886123
George Hu <wuorsut@gmail.com>
2023-04-20 15:36:10 +0800

    thread-list & post-list: fix scrolling to end

--- pages/2b/thread-[id]-[page]-[threadListPage].vue
+++ pages/2b/thread-[id]-[page]-[threadListPage].vue
@@ -11,29 +11,29 @@ const route = useRoute();
 const isShowImg = ref(false);
 
 const threadKey = `thread-${route.params.id}-page`;
-let page = parseInt(localStorage.getItem(threadKey) || route.params.page);
+let pageNum = parseInt(localStorage.getItem(threadKey) || route.params.page);
 
-let scrollTopPage = page - 1;
-let scrollBottomPage = page;
+let scrollTopPage = pageNum - 1;
+let scrollBottomPage = pageNum;
 let loading = true;
 
-let threadOrigUrl = apiWebUrl + `thread-${route.params.id}-${page}-${route.params.threadListPage}.html`
+let threadOrigUrl = apiWebUrl + `thread-${route.params.id}-${pageNum}-${route.params.threadListPage}.html`
 
 const load = async ($state, isTop=false) => {
   console.log("loading...");
   
   if(isTop){
-    page = scrollTopPage;
+    pageNum = scrollTopPage;
   }else{
-    page = scrollBottomPage;
+    pageNum = scrollBottomPage;
   }
   try {
-    localStorage.setItem(threadKey, page);
+    localStorage.setItem(threadKey, pageNum);
 
     let formData = new FormData();
     formData.append('sid', localStorage.getItem("sid"));
     formData.append('tid', route.params.id);
-    formData.append('pageNo', page);
+    formData.append('pageNo', pageNum);
 
     const response = await fetch(apiPostList, {
       method: "POST",
@@ -47,21 +47,23 @@ const load = async ($state, isTop=false) => {
     }
 
     console.log(data)
-    
+    if(isTop){
+      allPosts.value.unshift(data.data.list);
+      scrollTopPage = pageNum - 1;
+    }else{
+      allPosts.value.push(data.data.list);
+      scrollBottomPage = pageNum + 1;
+    }
+
     // There are 30 posts in one page if it's not end
-    if (data.data.list < 30) {
+    if (data.data.list.length < 30) {
       $state.complete();
     } else {
-      if(isTop){
-        allPosts.value.unshift(data.data.list);
-        scrollTopPage = page - 1;
-      }else{
-        allPosts.value.push(data.data.list);
-        scrollBottomPage = page + 1;
-      }
       $state.loaded();
-      loading = false;
     }
+
+    loading = false;
+
   } catch (error) {
     console.log(error);
     $state.error();
@@ -91,8 +93,8 @@ function toggleShowImg() {
             hover:bg-blue-400 hover:text-blue-100">{{ isShowImg ? "Hide" : "Show" }} Img</button>
     </Menu>
     <InfiniteLoading v-if="scrollTopPage > 0 && !loading" top=true @infinite="load($event, true)" />
-    <div class="border-b-2 w-full" v-for="block in allPosts">
-      <div class="my-6" v-for="post in block">
+    <div class="border-b-2 w-full" v-for="page in allPosts">
+      <div class="my-6" v-for="post in page">
         <div class="my-1 flex gap-2 font-medium">
           <a :href="apiBaseUrl + post.authorid" target="_blank">{{ post.author }}</a>
           <div>{{ getPostDate(post.dateline) }}</div>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@ import InfiniteLoading from "v3-infinite-loading";
 import "v3-infinite-loading/lib/style.css";
 let allThreads = ref([]);
 
-let page = parseInt(localStorage.getItem("thread-list-page")) || 1;
-let scrollTopPage = page - 1;
-let scrollBottomPage = page;
+let pageNum = parseInt(localStorage.getItem("thread-list-page")) || 1;
+let scrollTopPage = pageNum - 1;
+let scrollBottomPage = pageNum;
 let threadOrigUrl;
 
 const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
@@ -15,32 +15,33 @@ const load = async ($state, isTop=false) => {
   console.log("loading...");
 
   if(isTop){
-    page = scrollTopPage;
+    pageNum = scrollTopPage;
   }else{
-    page = scrollBottomPage;
+    pageNum = scrollBottomPage;
   }
   try {
-    localStorage.setItem("thread-list-page", page);
-    threadOrigUrl = getApiThreadList(6, page);
+    localStorage.setItem("thread-list-page", pageNum);
+    threadOrigUrl = getApiThreadList(6, pageNum);
 
     const response = await fetch(threadOrigUrl);
     const data = await response.json();
     console.log(data);
 
+    if(isTop){
+      allThreads.value.unshift(data.Variables.forum_threadlist);
+      scrollTopPage = pageNum - 1;
+    }else{
+      allThreads.value.push(data.Variables.forum_threadlist);
+      scrollBottomPage = pageNum + 1;
+    }
+
     // There are 50 threads in one page if it's not end
     if (data.Variables.forum_threadlist < 50 ) {
       $state.complete();
     } else {
-      if(isTop){
-        allThreads.value.unshift(data.Variables.forum_threadlist);
-        scrollTopPage = page - 1;
-      }else{
-        allThreads.value.push(data.Variables.forum_threadlist);
-        scrollBottomPage = page + 1;
-      }
       $state.loaded();
-      loading = false;
     }
+    loading = false;
   } catch (error) {
     $state.error();
   }
@@ -52,8 +53,8 @@ const load = async ($state, isTop=false) => {
 <template>
   <div class="m-4 text-slate-600 flex flex-col items-center" >
     <InfiniteLoading v-if="scrollTopPage > 0 && !loading" top=true @infinite="load($event, true)" />
-    <div class="flex flex-col items-center border-b-2 w-full" v-for="block in allThreads">
-      <div v-for="thread in block">
+    <div class="flex flex-col items-center border-b-2 w-full" v-for="page in allThreads">
+      <div v-for="thread in page">
         <a :href="getThreadLink(thread)">{{ thread.subject }}&nbsp;&nbsp;<b>{{ thread.replies }}</b></a>
       </div>
       <hr>
