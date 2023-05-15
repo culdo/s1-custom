a433ed6a806f8d88e46e839462b762065810dfe4
George Hu <wuorsut@gmail.com>
2023-05-15 13:35:25 +0800

    Fix datetime that display non-local time

--- utils/common.js
+++ utils/common.js
@@ -8,7 +8,14 @@ export const apiGetForums = apiBaseUrl + 'index.php?module=forumindex';
 
 export const getApiThreadList = (fid, page) => apiBaseUrl + `index.php?module=forumdisplay&version=4&filter=typeid&tpp=50&fid=${fid}&page=${page}`;
 export const getPostDate = (timestamp) => {
-    return new Date(timestamp * 1000).toISOString().slice(0, 16).replace('T', ' ')
+    let dateFormat = new Date(timestamp * 1000);
+
+    return dateFormat.getFullYear() + 
+            "-" + ("0" + (dateFormat.getMonth()+1)).slice(-2) + 
+            "-" + ("0" + dateFormat.getDate()).slice(-2) +
+            " "+("0" + dateFormat.getHours()).slice(-2)+
+            ":"+("0" + dateFormat.getMinutes()).slice(-2)+
+            ":"+("0" + dateFormat.getSeconds()).slice(-2);
 }
 export const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
 export const getThreadOrigUrl = (route, pageNum) => apiWebUrl + `thread-${route.params.id}-${pageNum.value}-${route.params.threadListPage}.html`
