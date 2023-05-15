1879d9d5a9a95d4cce3938a894ca7ccb810c9911
George Hu <wuorsut@gmail.com>
2023-05-15 13:38:25 +0800

    Postime: remove seconds

--- utils/common.js
+++ utils/common.js
@@ -14,8 +14,7 @@ export const getPostDate = (timestamp) => {
             "-" + ("0" + (dateFormat.getMonth()+1)).slice(-2) + 
             "-" + ("0" + dateFormat.getDate()).slice(-2) +
             " "+("0" + dateFormat.getHours()).slice(-2)+
-            ":"+("0" + dateFormat.getMinutes()).slice(-2)+
-            ":"+("0" + dateFormat.getSeconds()).slice(-2);
+            ":"+("0" + dateFormat.getMinutes()).slice(-2);
 }
 export const getThreadLink = (thread) => `2b/thread-${thread.tid}-1-1`
 export const getThreadOrigUrl = (route, pageNum) => apiWebUrl + `thread-${route.params.id}-${pageNum.value}-${route.params.threadListPage}.html`
