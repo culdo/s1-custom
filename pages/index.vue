a1e323c584913556fb4d4cddc678aa2512ec2445
George Hu <wuorsut@gmail.com>
2023-07-13 15:08:58 +0800

    thread list: show replies diff when clicked

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@ const response = await fetch(apiGetForums);
 const data = await response.json();
 const totalThreads = parseInt(data.Variables.forumlist[1].threads);
 
+const setReplies = (thread) => {
+  thread.clicked = true
+}
+
 const getPrevReplies = (thread) => {
   return parseInt(localStorage.getItem(`${thread.tid}-replies`))
 }
@@ -35,7 +39,9 @@ async function fetcher(pageNum) {
     <template v-slot:content="props">
       <div class="flex flex-col items-center border-b-2 w-full" v-for="page in props.allItemList">
         <div v-for="thread in page">
-          <a :href="getThreadLink(thread)" :class="'thread-title ' + (showRepliesDiff(thread) ? 'text-slate-400' : null)" >{{ thread.subject }}&nbsp;&nbsp;<b class="text-emerald-800">{{ thread.replies }} {{ showRepliesDiff(thread) }}</b></a>
+          <a target="_blank" :href="getThreadLink(thread)" @click="setReplies(thread)" :class="'thread-title ' + (showRepliesDiff(thread) ? 'text-slate-400' : null)" >
+            {{ thread.subject }}&nbsp;&nbsp;<b class="text-emerald-800">{{ thread.replies }} {{ thread.clicked ? "(+0)" : showRepliesDiff(thread) }}</b>
+          </a>
         </div>
         <hr>
       </div>
