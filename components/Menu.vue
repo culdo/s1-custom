edf2df4e07755d6fcdac7817c3957e40703c4ed8
George Hu <wuorsut@gmail.com>
2023-07-19 10:41:59 +0800

    menu: close menu when click outside

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,12 +1,20 @@
 <script setup>
 const showMenu = ref(false);
+const toggleBt = ref(null);
 
+document.addEventListener('click', e => {
+    const isClickInside = toggleBt.value.contains(e.target)
+
+  if (!isClickInside) {
+    showMenu.value = false;
+  }
+})
 </script>
 
 <template>
     <div class="inline-block sticky top-0 z-50 float-right">
         <!-- Dropdown toggle button -->
-        <button @click="showMenu = !showMenu" class="flex items-center p-2 bg-white rounded-md">
+        <button ref="toggleBt" @click="showMenu = !showMenu" class="flex items-center p-2 bg-white rounded-md">
             <svg class="w-5 h-5 dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                 <path
                     d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z">
